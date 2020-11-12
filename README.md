6118f297b8536f256c2eb598bc8d5e3c63a83f25
wesdeitch24@gmail.com <coded.journey@gmail.com>
2020-11-11 19:03:19 -0800

    Finished SSR app

---
+++ README.md
@@ -0,0 +1,12 @@
+### React Simple Server Side Rendering App
+
+#### A simple react app to learn to create a server side rendering application
+
+###### What I've Learned:
+
+1. How to create a server side rendering application
+1. Use Redux with SSR
+1. Use React Router with SSR
+1. Data Loading
+1. Authentication in SSR
+1. SEO support
---
+++ src/client/reducers/index.js
@@ -0,0 +1,11 @@
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
+import { combineReducers } from "redux";
+import userReducer from "./userReducer";
+import authReducer from "./authReducer";
+import adminsReducer from "./adminsReducer";
+
+export default combineReducers({
+  users: userReducer,
+  auth: authReducer,
+  admins: adminsReducer,
+});
